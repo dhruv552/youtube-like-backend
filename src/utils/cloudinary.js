468d6c94ec854ab7b4a749f98633b242abddec0c
dhruv552 <dhruvagrawal552@gmail.com>
2026-06-26 15:59:27 +0530

    integration of multer and cloudinary

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,33 @@
+import { v2 as cloudinary } from 'cloudinary';
+import fs from 'fs';
+
+// configure cloudinary 
+// Configuration
+cloudinary.config({ 
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET
+});
+
+const uploadOnCloudinary = async (localFilePath) => {
+    try {
+        if (!localFilePath) return null;
+        const response = await cloudinary.uploader.upload(localFilePath, {
+            resource_type: 'auto', // this will automatically detect the file type (image, video, etc.)
+        });
+        console.log('File uploaded to Cloudinary successfully, File Source: ' + response.url);
+        // Once the file is uploaded to Cloudinary, we can delete the local file
+        fs.unlinkSync(localFilePath);
+        return response;
+    } catch (error) {
+        console.error('Error uploading to Cloudinary:', error);
+        try {
+            fs.unlinkSync(localFilePath); // Attempt to delete the file even if upload fails
+        } catch (unlinkError) {
+            console.error('Error deleting local file:', unlinkError);
+        }
+        return null;
+    }
+};
+
+export { uploadOnCloudinary };
